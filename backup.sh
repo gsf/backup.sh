e7031bb0501eabb9bc5c91e3a4b8d6a6d6c63595
Gabriel Farrell <gsf747@gmail.com>
2012-06-26 04:19:43 -0400

    Cleanup and a bit more in the README

--- backup.sh
+++ backup.sh
@@ -3,46 +3,49 @@
 
 MAX_BAKS=10
 
+usage="usage: backup.sh <source> [dest]"
+
 # src is the source, e.g. example.com:/mnt/mirror/
-src="$1"
+case $1 in
+  '') echo $usage; exit 0 ;;
+  */) src="$1" ;; # assign if ends in a slash
+  *) src="$1/" ;; # add slash if it doesn't
+esac
 
 # don't do anything if no ssh connection
 src_host=${src%%:*}
 if ! ssh $src_host :; then exit 
 fi
 
-# bakdir is the path to the backup dir, e.g. backups/example/
+# dest is the path to the destination directory, e.g. backups/example/
 case $2 in 
-  '') bakdir="$PWD/" ;; # if empty assign PWD
-  /*/) bakdir="$2" ;; # if starts and ends in a slash assign
-  /*) bakdir="$2/" ;; # if no end slash add one
-  */) bakdir="$PWD/$2" ;; # if no start slash add PWD
-  *) bakdir="$PWD/$2/" ;; # if no start or end slash add all
+  '') dest="$PWD/" ;; # if empty assign PWD
+  /*/) dest="$2" ;; # if starts and ends in a slash assign
+  /*) dest="$2/" ;; # if no end slash add one
+  */) dest="$PWD/$2" ;; # if no start slash add PWD
+  *) dest="$PWD/$2/" ;; # if no start or end slash add all
 esac
 
-echo "Backing up to $bakdir...",
+name=`date --iso-8601=seconds`
 
-mkdir -p "$bakdir"
+echo "Backing up to $dest$name..."
 
-baks=`ls "$bakdir"`
+mkdir -p "$dest"
 
+baks=`ls "$dest"`
 i=`echo $baks | wc -w`
-
 for b in $baks; do
-  #echo $b
   if [ $i -gt $MAX_BAKS ]; then
-    rm -rf "$bakdir$b"
+    rm -rf "$dest$b"
   fi
   if [ $i -eq 1 ]; then
-    linkdest="$bakdir$b"
+    linkdest="$dest$b"
   fi
   i=$((i-1))
 done
 
-name=`date --iso-8601=seconds`
-
 if [ -n $linkdest ]; then
-  rsync -az --del "$src" "$bakdir$name"
+  rsync -az --del "$src" "$dest$name"
 else
-  rsync -az --del --link-dest=$linkdest "$src" "$bakdir$name"
+  rsync -az --del --link-dest=$linkdest "$src" "$dest$name"
 fi
