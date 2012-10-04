b51924a4cdfbf1b03175382ca00d4fb1ad4f1414
Gabriel Farrell <g@grrawr.com>
2012-10-04 15:33:34 -0400

    Added options and fixed backward linkdest check

--- backup.sh
+++ backup.sh
@@ -1,20 +1,45 @@
 #!/bin/sh
 # See http://www.mikerubel.org/computers/rsync_snapshots/
 
-MAX_BAKS=10
+verbose=false
+dry=false
+max=10
 
-usage="usage: backup.sh <source> [dest]"
+# get options
+while getopts ":nvm:" opt; do
+  case $opt in
+    n) dry=true ;;
+    v) verbose=true ;;
+    m) max=$OPTARG ;;
+  esac
+done
+
+# shift positional parameter past options
+shift $((OPTIND-1))
 
 # src is the source, e.g. example.com:/mnt/mirror/
 case $1 in
-  '') echo $usage; exit ;;
+  '') 
+    cat <<EOF
+usage: backup.sh [options] source [dest]
+  -n      dry run (don't actually rsync anything)
+  -v      verbose
+  -m max  set max number of backups to something other than 10
+EOF
+    exit ;;
   */) src="$1" ;; # assign if ends in a slash
   *) src="$1/" ;; # add slash if it doesn't
 esac
 
+if $dry && $verbose; then
+  echo "this is a dry run"
+fi
+
 # don't do anything if no ssh connection
-#src_host=${src%%:*}
 src_host=`echo $src | sed 's/:.*$//'`
+if $verbose; then
+  echo "checking ssh connection"
+fi
 if ! ssh $src_host :; then 
   exit 
 fi
@@ -32,15 +57,19 @@ name=`date --iso-8601=seconds`
 # replace colons to avoid path name issues
 name=`echo $name | sed 'y/:/./'`
 
-echo "Backing up to $dest$name..."
+# make parent directories if needed
+if $verbose; then
+  mkdir -pv "$dest"
+else
+  mkdir -p "$dest"
+fi
 
-mkdir -p "$dest"
-
+# loop through backups from oldest to most recent
 baks=`ls "$dest"`
 i=`echo $baks | wc -w`
 for b in $baks; do
   # remove old backups
-  if [ $i -gt $MAX_BAKS ]; then
+  if [ $i -gt $max ]; then
     rm -rf "$dest$b"
   fi
   if [ $i -eq 1 ]; then
@@ -49,8 +78,18 @@ for b in $baks; do
   i=$((i-1))
 done
 
+# set up rsync options
+opts='-az'
+if $dry; then
+  opts="${opts}n"
+fi
+if $verbose; then
+  opts="${opts}v"
+fi
+
+# if the linkdest variable isn't empty use it
 if [ -n $linkdest ]; then
-  rsync -az --del "$src" "$dest$name"
+  rsync $opts --del --link-dest=$linkdest "$src" "$dest$name"
 else
-  rsync -az --del --link-dest=$linkdest "$src" "$dest$name"
+  rsync $opts --del "$src" "$dest$name"
 fi
